f85dd204306f721d3887a61d9cbd71ea57edb12c
atsumo <bachisuta@gmail.com>
2014-08-29 17:52:33 +0900

    add sample data

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 "use strict";
 
+var paths = {
+  scripts: [
+    'bower_components/bootstrap/dist/js/*.js', '!bower_components/bootstrap/dist/js/*.min.js',
+    'bower_components/jquery/dist/*.js', '!bower_components/jquery/dist/*.min.js',
+    'bower_components/lodash/dist/*.js', '!bower_components/lodash/dist/*.min.js'
+  ],
+  images: [''],
+  styles: ['']
+};
+
 var gulp = require('gulp');
 //rename
 var rename = require('gulp-rename');
@@ -7,10 +17,17 @@ var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
 //js minify
 var uglify = require('gulp-uglify');
+//del
+var del = require('del');
+
+//clean
+gulp.task('clean', function(callback) {
+  del(['dist'], callback);
+});
 
 // https://github.com/chilijung/gulp-cssmin/
-gulp.task('css', function() {
-  gulp.src('src/**/*.css')
+gulp.task('styles', function() {
+  return gulp.src(paths.styles)
     .pipe(cssmin())
     // min
     .pipe(rename({ suffix: '.min' }))
@@ -18,10 +35,17 @@ gulp.task('css', function() {
 });
 
 //js minify
-gulp.task('compress', function() {
-  gulp.src('src/**/*.js')
+gulp.task('scripts', function() {
+  console.log('start scripts');
+  return gulp.src(paths.scripts)
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
 
+//images
+gulp.task('images', function() {
+  console.log('TODO images', paths.images);
+});
+
+gulp.task('default', ['clean','scripts', 'styles', 'images']);
